2e7c392ae6a0f388de3490fedb1f1e5df223c26e
Tanishq Kancharla <tanishqkancharla3@gmail.com>
2022-05-07 01:12:16 -0400

    types

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,9 @@
-import type { Config } from '@jest/types';
+import type { Config } from "@jest/types";
 
 // Sync object
 const config: Config.InitialOptions = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  verbose: true,
+	preset: "ts-jest",
+	testEnvironment: "node",
+	verbose: true,
 };
 export default config;
