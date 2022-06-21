fdcf83446dea068301fb0381fe9fb78cac0624a0
Tanishq Kancharla <tanishqkancharla3@gmail.com>
2022-06-21 01:17:34 -0400

    cleanup

--- build/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { Parser } from "./Parser";
-import { ParseFailure, ParseResult, ParseSuccess } from "./ParseResult";
-import { ParserStream } from "./ParserStream";
-import { between, isParseFailure, isParseSuccess, line, logResult, lookahead, maybe, nOrMore, notChars, oneOf, oneOrMore, prefix, sequence, str, suffix, takeUntilAfter, zeroOrMore } from "./parseUtils";
-export { str, logResult, notChars, nOrMore, zeroOrMore, oneOrMore, oneOf, sequence, lookahead, maybe, between, prefix, suffix, takeUntilAfter, Parser, ParseFailure, ParseResult, ParseSuccess, ParserStream, isParseFailure, isParseSuccess, line, };
--- jest.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-// Sync object
-var config = {
-    preset: "ts-jest",
-    testEnvironment: "node",
-    verbose: true
-};
-exports["default"] = config;
