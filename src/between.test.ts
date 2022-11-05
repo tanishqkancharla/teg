dfaa4576f3715be5cc9b1b213ec2c1f31711260a
Tanishq Kancharla <tanishqkancharla3@gmail.com>
2022-11-05 15:41:00 -0700

    cleanup

--- src/between.test.ts
+++ src/between.test.ts
@@ -5,19 +5,11 @@ import { testParser } from "./testParser";
 describe("between", () => {
 	const parser = testParser(between(char("a"), char("b"), char("c")));
 
-	it("works", () => {
-		parser.parses("abc", "b");
-	});
+	it("works", () => parser.parses("abc", "b"));
 
-	it("fails when prefix doesn't match", () => {
-		parser.fails("dbc");
-	});
+	it("fails when prefix doesn't match", () => parser.fails("dbc"));
 
-	it("fails when middle doesn't match", () => {
-		parser.fails("adc");
-	});
+	it("fails when middle doesn't match", () => parser.fails("adc"));
 
-	it("fails when suffix doesn't match", () => {
-		parser.fails("abd");
-	});
+	it("fails when suffix doesn't match", () => parser.fails("abd"));
 });
